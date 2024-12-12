013b30649d8bdd9339f4908e5a3589b5aaff48fd
Linzheng Tang <tanglzh2022@shanghaitech.edu.cn>
2024-12-12 15:07:00 +0000

    feat: implement agent thread for concurrent state collection and action testing

--- main.py
+++ main.py
@@ -11,17 +11,33 @@ def create_server_thread():
     return thread
 
 
+def create_agent_thread():
+
+    def start_agent():
+        while True:
+            try:
+                StateCollector.collect_state()
+                random_action_test()
+            except KeyboardInterrupt:
+                logger.info("Shutting down agent...\n")
+                break
+
+    thread = Thread(target=start_agent(), daemon=True)
+    thread.start()
+    return thread
+
+
 if __name__ == "__main__":
     server_thread = create_server_thread()
+    agent_thread = create_agent_thread()
 
-    while True:
-        try:
-            StateCollector.collect_state()
-            random_action_test()
-        except KeyboardInterrupt:
-            try:
-                server_thread.join()
-            except KeyboardInterrupt:
-                logger.info("Shutting down server...\n")
-                server.stop()
-                exit(0)
+    agent_thread.join()
+    logger.info("Agent stopped\n")
+
+    try:
+        server_thread.join()
+    except KeyboardInterrupt:
+        logger.info("Shutting down server...\n")
+        server.stop()
+
+    exit(0)

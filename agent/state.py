bdcf30239a3b6a173fecf205719b518ede51215b
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-12 13:25:33 +0000

    feat: refactor action handling and state collection; add GameAction and PawnAction dataclasses

--- agent/state.py
+++ agent/state.py
@@ -1,8 +1,6 @@
 from utils.server import server
 from utils.logger import logger
 
-from threading import Thread
-
 from dataclasses import dataclass, field
 from typing import Dict
 
@@ -69,21 +67,18 @@ class GameState:
 
 class StateCollector:
     current_state = None
-    thread = None
 
     @classmethod
-    def initialize(cls) -> None:
-        cls._start_listen()
-
-    @classmethod
-    def _collect_state(cls) -> None:
+    def collect_state(cls) -> None:
         while True:
             if server.message_queue.qsize() > 0:
-                message = server.message_queue.get()
-                cls.current_state = GameState.from_dict(message)
-                logger.debug(f"Collected game state at tick {cls.current_state.tick}\n")
-
-    @classmethod
-    def _start_listen(cls) -> None:
-        cls.thread = Thread(target=cls._collect_state, daemon=True)
-        cls.thread.start()
+                # Peek at message without removing it
+                message = server.message_queue.queue[0]
+                # Only get the message if it's a state message
+                if "GameState" == message["Type"]:
+                    message = server.message_queue.get()
+                    cls.current_state = GameState.from_dict(message["Data"])
+                    logger.debug(
+                        f"Collected game state at tick {cls.current_state.tick}\n"
+                    )
+                    break

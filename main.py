68991fe1018612610065978f7521618aa719a16b
zivmax <zivmax@foxmail.com>
2024-12-14 05:43:10 +0800

    feat: refactor RandomAgent to accept GameState as input and update action selection logic

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import sys
 # Global flag to indicate if the program should stop
 stop_event = Event()
 
-def create_server_thread():        
+
+def create_server_thread():
     thread = Thread(target=server.start, daemon=True)
     thread.start()
     return thread
@@ -19,22 +20,25 @@ def create_server_thread():
 def create_agent_thread():
 
     def start_agent():
+        agent = RandomAgent()
         while True:
-            StateCollector.collect_state()
+            StateCollector.receive_state()
             if StateCollector.current_state.status == GameStatus.RUNNING:
-                agent = RandomAgent()
-                action = agent.act()
-
+                action = agent.act(StateCollector.current_state)
                 message = {
                     "Type": "GameAction",
                     "Data": dict(action),
                 }
-                for client in server.clients:
-                    server.send_to_client(client, message)
+                server.send_to_client(server.client, message)
 
                 logger.info(
-                    f"Sent actions to clients at tick {StateCollector.current_state.tick}\n"
+                    f"\tSent actions to clients at tick {StateCollector.current_state.tick}\n"
                 )
+            else:
+                logger.info("\tEpisode Ends.\n")
+                message = {"Type": "Reset", "Data": None}
+                StateCollector.current_state = None
+                server.send_to_client(server.client, message)
 
     thread = Thread(target=start_agent(), daemon=True)
     thread.start()
@@ -42,10 +46,11 @@ def create_agent_thread():
 
 
 def signal_handler(sig, frame):
-    logger.info("Stopping threads...\n")
+    logger.info("\tStopping threads...\n")
     stop_event.set()
     server.stop()
     sys.exit(0)
+
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)

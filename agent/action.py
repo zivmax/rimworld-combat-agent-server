24f19b39c309f1d32b21e386306e29980e931420
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-13 23:46:46 +0800

    refacotr: refactor the project structure, support multi type of agent.

--- agent/action.py
+++ agent/action.py
@@ -1,9 +1,3 @@
-from utils.server import server
-from utils.logger import logger
-from utils.json import to_json
-from agent.state import StateCollector
-
-from random import randint
 from dataclasses import dataclass
 from typing import Dict
 
@@ -27,36 +21,3 @@ class GameAction:
     def __iter__(self):
         yield ("PawnActions", {k: dict(v) for k, v in self.pawn_actions.items()})
 
-
-def random_action_test() -> None:
-
-    state = StateCollector.current_state
-
-    # From GameState to fetch the labels of the ally pawn
-    ally_pawn_labels = []
-    for pawn in state.pawn_states.values():
-        if pawn.is_ally:
-            ally_pawn_labels.append(pawn.label)
-
-    # For each ally pawn, create a random action
-    pawn_actions = {}
-    for label in ally_pawn_labels:
-        pawn_actions[label] = PawnAction(
-            label=label,
-            x=randint(0, state.map_state.width),
-            y=randint(0, state.map_state.height),
-        )
-
-    game_action = GameAction(pawn_actions)
-
-    message = {
-        "Type": "GameAction",
-        "Data": dict(game_action),
-    }
-    for client in server.clients:
-        server.send_to_client(client, message)
-
-    logger.info(f"Sent random actions to clients at tick {state.tick}\n")
-    logger.debug(
-        f"Random actions (tick {state.tick}): \n{to_json(game_action, indent=2)}\n"
-    )
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from utils.server import server
 from utils.logger import logger
-from agent.state import StateCollector
-from agent.action import random_action_test
+from agent.state import StateCollector, GameStatus
+from agent.random import RandomAgent
+
 from threading import Thread
 
 
-def create_server_thread():
+def create_server_thread():        
     thread = Thread(target=server.start, daemon=True)
     thread.start()
     return thread
@@ -15,12 +16,21 @@ def create_agent_thread():
 
     def start_agent():
         while True:
-            try:
-                StateCollector.collect_state()
-                random_action_test()
-            except KeyboardInterrupt:
-                logger.info("Shutting down agent...\n")
-                break
+            StateCollector.collect_state()
+            if StateCollector.current_state.status == GameStatus.RUNNING:
+                agent = RandomAgent()
+                action = agent.act()
+
+                message = {
+                    "Type": "GameAction",
+                    "Data": dict(action),
+                }
+                for client in server.clients:
+                    server.send_to_client(client, message)
+
+                logger.info(
+                    f"Sent actions to clients at tick {StateCollector.current_state.tick}\n"
+                )
 
     thread = Thread(target=start_agent(), daemon=True)
     thread.start()
@@ -31,13 +41,12 @@ if __name__ == "__main__":
     server_thread = create_server_thread()
     agent_thread = create_agent_thread()
 
-    agent_thread.join()
-    logger.info("Agent stopped\n")
-
     try:
+        agent_thread.join()
         server_thread.join()
     except KeyboardInterrupt:
-        logger.info("Shutting down server...\n")
+        logger.info("Agent stopped\n")
         server.stop()
+        logger.info("Server stopped\n")
 
     exit(0)

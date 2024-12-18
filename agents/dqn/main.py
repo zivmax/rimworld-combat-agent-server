dcd4354f210b76c62629f40f047e98c8490d09ee
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-12-18 11:30:02 +0000

    fix: finished load state dict

--- agents/dqn/main.py
+++ agents/dqn/main.py
@@ -54,7 +54,18 @@ def main():
                 agent.save(os.path.join(save_dir, f"episode_{episode + 1}.pth"))
     else:
         agent.load(LOAD_PATH)
+        obs, info = env.reset()
+        done = False
+        total_reward = 0
 
+        while not done:
+            action = agent.act(obs, info)
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            agent.step(obs, action, reward, next_obs, done)
+            obs = next_obs
+            total_reward += reward
+        logger.info(f"Total reward after loading: {total_reward}")
     env.close()
 
     draw(env, "agents/dqn/plot/episode_stats.png")

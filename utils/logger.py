ecf36dceec9465a679c56b768fd41c92fc9cc32f
Linzheng Tang <tanglzh2022@shanghaitech.edu.cn>
2024-12-18 13:01:27 +0000

    fix: fix the functionality of the logger util.

--- utils/logger.py
+++ utils/logger.py
@@ -10,13 +10,14 @@ def get_cli_logger(logger_name: str, level) -> logging.Logger:
     """
 
     # Configure logging with colored formatter
-    handler = logging.StreamHandler()
-    handler.setFormatter(
+    cli_handler = logging.StreamHandler()
+    cli_handler.setFormatter(
         ColoredFormatter("%(asctime)s\t%(levelname)s\t%(filename)s\t%(message)s")
     )
-    logging.basicConfig(level=logging.INFO, handlers=[handler])
     logger = logging.getLogger("cli-" + logger_name)
+    logger.addHandler(cli_handler)
     logger.setLevel(level)
+    logger.propagate = False
     return logger
 
 
@@ -26,10 +27,11 @@ def get_file_logger(logger_name: str, path: str, level) -> logging.Logger:
     """
     os.makedirs(os.path.dirname(path), exist_ok=True)
     file_handler = logging.FileHandler(path)
-    file_handler.setLevel(level)
     file_handler.setFormatter(
         logging.Formatter("%(asctime)s\t%(levelname)s\t%(filename)s\t%(message)s")
     )
     logger = logging.getLogger("file-" + logger_name)
     logger.addHandler(file_handler)
+    logger.setLevel(level)  # Add this line
+    logger.propagate = False
     return logger

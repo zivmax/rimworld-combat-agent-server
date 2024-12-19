c6cc077dcb2e9ee3c88b760c94e6328b10addfb9
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-12-19 07:53:55 +0000

    feat: add differential reward count

--- agents/dqn/hyper_params.py
+++ agents/dqn/hyper_params.py
@@ -1,11 +1,11 @@
 # this page is explicitly used fr storing trainning hyperparams
 
-EPSILON = {"START": 1, "FINAL": 0.01, "DECAY": 0.995}
+EPSILON = {"START": 1.0, "FINAL": 0.2, "DECAY": 0.995}
 HIDDEN_SIZE1 = 256
 HIDDEN_SIZE2 = 128
 TARGET_UPDATE = 10
 
-MEMORY_SIZE = 500000
+MEMORY_SIZE = 10000
 DEVICE = "cuda"
 
 BATCH_SIZE = 64
@@ -13,13 +13,13 @@ BATCH_SIZE = 64
 BATCH_X = 8 
 BATCH_Y = 8 
 
-GAMMA = 0.99
+GAMMA = 0.94
 
 LEARNING_RATE = 0.01
 
-N_EPISODES = 200
+N_EPISODES = 1000
 
-EPISOLD_SAVE_INTERVAL = 50
+EPISOLD_SAVE_INTERVAL = 100
 
 EPISOLD_LOG_INTERVAL = 10
 
@@ -28,15 +28,15 @@ RE_TRAIN = True
 LOAD_PATH = "agents/dqn/model_pth/dqn_model_episode_1000.pth"
 
 OPTIONS = {
-    "interval": 2.0,
+    "interval": 1.0,
     "speed": 3,
     "action_range": 4,
     "is_remote": False,
     "rewarding": {
         "original": 0,
         "ally_down": -8,
-        "enemy_down": 10,
-        "ally_danger": -0.7,
-        "enemy_danger": 0.9,
+        "enemy_down": 15,
+        "ally_danger": -1.2,
+        "enemy_danger": 1.5,
     },
 }

96ad28e70edb901dbea5f9a20ccbe6c49d00116d
zivmax <tanglzh2022@shanghaitech.edu.cn>
2024-12-19 13:59:24 +0000

    fix: update training logic and hyperparameters for DQNAgent

--- agents/dqn/hyper_params.py
+++ agents/dqn/hyper_params.py
@@ -10,8 +10,8 @@ DEVICE = "cuda"
 
 BATCH_SIZE = 64
 
-BATCH_X = 8 
-BATCH_Y = 8 
+BATCH_X = 8
+BATCH_Y = 8
 
 GAMMA = 0.94
 
@@ -25,20 +25,20 @@ EPISOLD_LOG_INTERVAL = 10
 
 LOAD_TEST_EPISODES = 50
 
-RE_TRAIN = False
+TRAINING = True
 
 LOAD_PATH = "agents/dqn/models/2024-12-19_20:08:09/episode_25.pth"
 
 OPTIONS = {
     "interval": 1.0,
-    "speed": 3,
+    "speed": 1,
     "action_range": 4,
     "is_remote": False,
     "rewarding": {
         "original": 0,
         "ally_down": -8,
         "enemy_down": 15,
-        "ally_danger": -1.2,
+        "ally_danger": 0.2,
         "enemy_danger": 1.5,
     },
 }

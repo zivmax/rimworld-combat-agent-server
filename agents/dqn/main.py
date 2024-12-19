18cd1148261f8fe7659ef0e72ebde67a1b3b8079
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-12-19 12:28:00 +0000

    fix:load and retrain process

--- agents/dqn/main.py
+++ agents/dqn/main.py
@@ -11,7 +11,7 @@ from utils.json import to_json
 
 from .logger import logger
 from .hyper_params import N_EPISODES, EPISOLD_LOG_INTERVAL, EPISOLD_SAVE_INTERVAL
-from .hyper_params import RE_TRAIN, OPTIONS, LOAD_PATH
+from .hyper_params import RE_TRAIN, OPTIONS, LOAD_PATH, LOAD_TEST_EPISODES
 
 
 def main():
@@ -26,36 +26,14 @@ def main():
         action_space=env.action_space,
     )
 
-    if RE_TRAIN:
+    save_dir = f"agents/dqn/models/{timestamp}" 
 
-        save_dir = f"agents/dqn/models/{timestamp}"
-
-        num_episodes = N_EPISODES
-        for episode in tqdm(range(num_episodes), desc="Training Episodes"):
-            obs, info = env.reset()
-            done = False
-            total_reward = 0
-
-            while not done:
-                action = agent.act(obs, info)
-                next_obs, reward, terminated, truncated, info = env.step(action)
-                done = terminated or truncated
-
-                agent.step(obs, action, reward, next_obs, done)
-                obs = next_obs
-                total_reward += reward
-
-            if (episode + 1) % EPISOLD_LOG_INTERVAL == 0:
-                logger.debug(f"\tFor episode {episode + 1}, reward: {total_reward}")
-
-            if (episode + 1) % EPISOLD_SAVE_INTERVAL == 0:
-                agent.save(os.path.join(save_dir, f"episode_{episode + 1}.pth"))
-    else:
-        agent.load(LOAD_PATH)
+    num_episodes = N_EPISODES if RE_TRAIN else LOAD_TEST_EPISODES
+    for episode in tqdm(range(num_episodes), desc="Training Episodes" if RE_TRAIN else "Testing Episodes"):
+        agent.load(LOAD_PATH) if not RE_TRAIN else None
         obs, info = env.reset()
         done = False
         total_reward = 0
-
         while not done:
             action = agent.act(obs, info)
             next_obs, reward, terminated, truncated, info = env.step(action)
@@ -63,7 +41,12 @@ def main():
             agent.step(obs, action, reward, next_obs, done)
             obs = next_obs
             total_reward += reward
-        logger.info(f"\tTotal reward after loading: {total_reward}")
+        logger.info(f"\tTotal reward for episode {episode + 1}: {total_reward}") if not RE_TRAIN else None
+        if (episode + 1) % EPISOLD_LOG_INTERVAL == 0 and RE_TRAIN:
+            logger.debug(f"\tFor episode {episode + 1}, reward: {total_reward}")
+        if (episode + 1) % EPISOLD_SAVE_INTERVAL == 0 and RE_TRAIN:
+            agent.save(os.path.join(save_dir, f"episode_{episode + 1}.pth"))
+            
     env.close()
 
     draw(env, "agents/dqn/plot/episode_stats.png")

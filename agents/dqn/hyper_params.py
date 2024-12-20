9206240d488484a231b1f606da8d54d2d6b20baa
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-12-20 01:05:29 +0000

    feat: add continue training

--- agents/dqn/hyper_params.py
+++ agents/dqn/hyper_params.py
@@ -1,6 +1,6 @@
 # this page is explicitly used fr storing trainning hyperparams
 
-EPSILON = {"START": 1.0, "FINAL": 0.2, "DECAY": 0.995}
+EPSILON = {"START": 1.0, "FINAL": 0.1, "DECAY": 0.995}
 HIDDEN_SIZE1 = 256
 HIDDEN_SIZE2 = 128
 TARGET_UPDATE = 10
@@ -13,13 +13,13 @@ BATCH_SIZE = 64
 BATCH_X = 8
 BATCH_Y = 8
 
-GAMMA = 0.94
+GAMMA = 0.96
 
 LEARNING_RATE = 0.01
 
 N_EPISODES = 1000
 
-EPISOLD_SAVE_INTERVAL = 25
+EPISOLD_SAVE_INTERVAL = 100
 
 EPISOLD_LOG_INTERVAL = 10
 
@@ -27,12 +27,13 @@ LOAD_TEST_EPISODES = 50
 
 TRAINING = True
 
-LOAD_PATH = "agents/dqn/models/2024-12-19_20:08:09/episode_25.pth"
-
+LOAD_PATH = "agents/dqn/models/2024-12-19_22:58:36/episode_400.pth"
+CONTINUE_TRAINING_PATH = "agents/dqn/models/2024-12-19_22:58:36/episode_400.pth"
+CONTINUE_NUM = 400
 OPTIONS = {
     "interval": 1.0,
-    "speed": 1,
-    "action_range": 4,
+    "speed": 3,
+    "action_range": 3,
     "is_remote": False,
     "rewarding": {
         "original": 0,

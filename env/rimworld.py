1680188ee1ba97a804c7f6d7693bf02d93810337
Linzheng Tang <tanglzh2022@shanghaitech.edu.cn>
2024-12-22 07:56:03 +0000

    refactor: Move distance calculation functions to math module

--- env/rimworld.py
+++ env/rimworld.py
@@ -200,12 +200,6 @@ class RimWorldEnv(gym.Env):
         self._update_enemies()
         self._update_action_mask()
 
-    def _Mannhatan_dist(self, loc1: Loc, loc2: Loc) -> float:
-        return abs(loc1.x - loc2.x) + abs(loc1.y - loc2.y)
-
-    def _Eclidean_dist(self, loc1: Loc, loc2: Loc) -> float:
-        return np.sqrt((loc1.x - loc2.x) ** 2 + (loc1.y - loc2.y) ** 2)
-
     def _clamp(self, loc: Loc) -> Loc:
         return Loc(
             max(min(loc.x, self._map.width - 1), 0),

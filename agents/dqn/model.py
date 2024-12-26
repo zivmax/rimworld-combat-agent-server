fix: fix observation normalization in DQNAgent and fix frame stacking

agents/dqn/model.py
@@ -62,19 +62,9 @@ class DQN(nn.Module):
         # Convert the entire tensor to float
         x = x.float()
 
-        # Normalize each layer separately
-        x[:, 0] = x[:, 0] / torch.tensor(
-            self.obs_space.high[0], device=x.device
-        )  # Ally pos layer
-        x[:, 1] = x[:, 1] / torch.tensor(
-            self.obs_space.high[1], device=x.device
-        )  # Enemy pos layer
-        x[:, 2] = x[:, 2]  # Cover pos layer (0-1)
-        x[:, 3] = x[:, 3]  # Aiming layer (0-1)
-        x[:, 4] = x[:, 4]  # Status layer (0-1)
-        x[:, 5] = x[:, 5] / torch.tensor(
-            self.obs_space.high[5], device=x.device
-        )  # Danger layer (0-100)
+        x = x / (torch.tensor(self.obs_space.high, device=x.device)).repeat(
+            x.shape[0], *(len(self.obs_space.high.shape) * [1])
+        )
 
         x = self.conv(x)
         x = x.view(x.size(0), -1)  # Flatten
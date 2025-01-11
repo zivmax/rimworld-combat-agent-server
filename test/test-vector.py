50bd890993bea9117a01fa521fc2bdf4c92f54c1
zivmax <tanglzh2022@shanghaitech.edu.cn>
2025-01-11 17:43:51 +0000

    refactor: streamline environment setup and cleanup in test-vector.py

--- test/test-vector.py
+++ test/test-vector.py
@@ -15,59 +15,49 @@ def handle_keyboard_interrupt(signum, frame):
 
 signal.signal(signal.SIGINT, handle_keyboard_interrupt)
 
-try:
-    ENV_OPTIONS = EnvOptions(
-        action_range=1,
-        max_steps=800,
-        remain_still_threshold=100,
-        rewarding=EnvOptions.Rewarding(
-            original=0,
-            win=50,
-            lose=-50,
-            ally_defeated=0,
-            enemy_defeated=0,
-            ally_danger=-200,
-            enemy_danger=200,
-            invalid_action=-0.25,
-            remain_still=0.05,
-        ),
-        game=GameOptions(
-            agent_control=True,
-            team_size=1,
-            map_size=15,
-            gen_trees=True,
-            gen_ruins=True,
-            random_seed=4048,
-            can_flee=False,
-            actively_attack=True,
-            interval=0.5,
-            speed=4,
-        ),
+ENV_OPTIONS = EnvOptions(
+    action_range=1,
+    max_steps=800,
+    remain_still_threshold=100,
+    rewarding=EnvOptions.Rewarding(
+        original=0,
+        win=50,
+        lose=-50,
+        ally_defeated=0,
+        enemy_defeated=0,
+        ally_danger=-200,
+        enemy_danger=200,
+        invalid_action=-0.25,
+        remain_still=0.05,
+    ),
+    game=GameOptions(
+        agent_control=True,
+        team_size=1,
+        map_size=15,
+        gen_trees=True,
+        gen_ruins=True,
+        random_seed=4048,
+        can_flee=False,
+        actively_attack=True,
+        interval=0.5,
+        speed=4,
+    ),
+)
+
+ports = [np.random.randint(10000, 20000) for _ in range(10)]
+envs = gym.vector.AsyncVectorEnv(
+    [
+        lambda port=port: gym.make(rimworld_env, options=ENV_OPTIONS, port=port)
+        for port in ports
+    ],
+    daemon=True,
+    shared_memory=True,
+)
+
+observations, infos = envs.reset(seed=42)
+for _ in tqdm.trange(1000):
+    observations, rewards, terminations, truncations, infos = envs.step(
+        envs.action_space.sample()
     )
 
-    ports = [np.random.randint(10000, 20000) for _ in range(10)]
-    envs = gym.vector.AsyncVectorEnv(
-        [
-            lambda port=port: gym.make(rimworld_env, options=ENV_OPTIONS, port=port)
-            for port in ports
-        ],
-        daemon=True,
-        shared_memory=True,
-    )
-
-    observations, infos = envs.reset(seed=42)
-    for _ in tqdm.trange(1000):
-        observations, rewards, terminations, truncations, infos = envs.step(
-            envs.action_space.sample()
-        )
-
-    print(observations, rewards, terminations, truncations, infos)
-
-except Exception as e:
-    print(f"An error occurred: {e}")
-    if "envs" in globals():
-        envs.close()
-
-finally:
-    if "envs" in globals():
-        envs.close()
+envs.close()

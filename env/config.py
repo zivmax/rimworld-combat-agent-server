db7a95be74c45dadc2f96fbcecd7026a8da82351
zivmax <tanglzh2022@shanghaitech.edu.cn>
2025-01-12 15:45:21 +0000

    fix: increase game restart interval to 500 episodes

--- env/config.py
+++ env/config.py
@@ -1,6 +1,6 @@
 import logging
 
-RESTART_INTERVAL = 300  # Restart the game every `RESTART_INTERVAL` episodes
+RESTART_INTERVAL = 500  # Restart the game every `RESTART_INTERVAL` episodes
 RESET_TIMEOUT = 300  # Timeout for state collection in seconds
 
 RIMWORLD_LOGGING_LEVEL = logging.INFO  # Logging level for RimWorld game process

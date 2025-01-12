6e171060946da715f922e9bf52b2aa75ccf6ec4e
zivmax <tanglzh2022@shanghaitech.edu.cn>
2025-01-12 16:10:37 +0000

    fix: update RESTART_INTERVAL to disable automatic restarts

--- env/config.py
+++ env/config.py
@@ -1,7 +1,10 @@
 import logging
 
-RESTART_INTERVAL = 300  # Restart the game every `RESTART_INTERVAL` episodes
-RESET_TIMEOUT = 300  # Timeout for state collection in seconds
+# Restart the game every `RESTART_INTERVAL` episodes
+RESTART_INTERVAL = 0 # 0 means never restart
+
+# Timeout for reset in seconds
+RESET_TIMEOUT = 300  
 
 RIMWORLD_LOGGING_LEVEL = logging.INFO  # Logging level for RimWorld game process
 SERVER_LOGGING_LEVEL = logging.INFO  # Logging level for the server process

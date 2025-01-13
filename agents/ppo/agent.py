fc1345cf7e11eb0361dd05d531a2a98a7f49f1ca
mike2367 <yuguo2022@shanghaitech.edu.cn>
2025-01-13 02:41:00 +0000

    fix: fix the plot/log part for ppo training, change structure to authetic ppo

--- agents/ppo/agent.py
+++ agents/ppo/agent.py
@@ -36,6 +36,9 @@ class PPOAgent:
         self.reuse_time = reuse_time
         self.state_values_store = []
         self.current_transitions = []
+        self.policy_loss_history = []
+        self.value_loss_history = []
+        self.loss_history = []
 
         self.policy = ActorCritic(obs_space, act_space).to(self.device)
         self.optimizer = optim.Adam(self.policy.parameters(), lr=lr)
@@ -90,7 +93,6 @@ class PPOAgent:
 
     def update(self) -> None:
         states = torch.stack([t.state for t in self.memory.transitions]).to(self.device)
-
         actions = torch.stack([t.action for t in self.memory.transitions]).to(
             self.device
         )
@@ -101,8 +103,11 @@ class PPOAgent:
             self.device
         )
         dones = torch.stack([t.done for t in self.memory.transitions]).to(self.device)
+
         returns, advantages = self.compute_advantages(rewards, dones)
+
         batch_size = min(self.batch_size, len(self.memory.transitions))
+
         dataset = torch.utils.data.TensorDataset(
             states, actions, old_log_probs, returns, advantages
         )
@@ -118,30 +123,36 @@ class PPOAgent:
                 batch_returns,
                 batch_advantages,
             ) = batch
+
             log_probs, entropy, state_values = self.policy.evaluate(batch_states)
 
             ratios = torch.exp(log_probs - batch_old_log_probs.detach())
-            # surr1 = ratios * batch_advantages
-            surr1 = log_probs.detach() * batch_advantages.detach()
-            # surr2 = (
-            #     torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip)
-            #     * batch_advantages
-            # )
+
+            surr1 = ratios * batch_advantages
+            surr2 = (
+                torch.clamp(ratios, 1 - self.eps_clip, 1 + self.eps_clip)
+                * batch_advantages
+            )
+
             entropy_bonus = self.entropy_coef * entropy
-            # actor_loss = -torch.min(surr1, surr2) - entropy_bonus
-            actor_loss = -surr1.mean() - entropy_bonus.mean()
-            # critic_loss = (
-            #     self.critic_coef * 0.5 * (batch_returns - state_values).pow(2)
-            # )
-            critic_loss = self.critic_coef * batch_advantages.detach().mean().pow(2)
-            # critic_loss = batch_advantages.pow(2)
+
+            actor_loss = -torch.min(surr1, surr2) - entropy_bonus
+
+            critic_loss = self.critic_coef * 0.5 * (batch_returns - state_values).pow(2)
 
             loss = actor_loss + critic_loss
+
             self.optimizer.zero_grad()
-            loss.backward()
+            loss.mean().backward()
+            torch.nn.utils.clip_grad_norm_(self.policy.parameters(), max_norm=1.0)
+
             self.optimizer.step()
 
-        self.state_values_store = []
+            self.policy_loss_history.append(actor_loss.detach().cpu().numpy())
+            self.value_loss_history.append(critic_loss.detach().cpu().numpy())
+            self.loss_history.append(loss.detach().cpu().numpy())
+
+        self.state_values_store.clear()
         self.memory.clear()
 
     def compute_advantages(self, rewards, dones):
@@ -164,9 +175,7 @@ class PPOAgent:
             previous_value = state_values[step]
             returns.insert(0, advantage + state_values[step])
 
-        advantages = torch.tensor(
-            advantages, dtype=torch.float32, requires_grad=True
-        ).to(self.device)
+        advantages = torch.tensor(advantages, dtype=torch.float32).to(self.device)
 
         returns = torch.tensor(returns, dtype=torch.float32).to(self.device)
         advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)

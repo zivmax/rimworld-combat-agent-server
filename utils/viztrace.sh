21b6eaa8d0b1468c5d9649eafb538ce5853327a8
zivmax <tanglzh2022@shanghaitech.edu.cn>
2025-01-13 07:30:42 +0000

    add viztrace scripts for performance tracing

---
+++ utils/viztrace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <module>"
+  exit 1
+fi
+
+# Assign the argument to a variable
+module="$1"
+
+# Generate the output file name by appending .json
+output_file="${module}.json"
+
+# Run viztracer with the generated output file and the command/script
+viztracer -o "$output_file" --max_stack_depth 10  --ignore_frozen --ignore_c_function -m "$module"

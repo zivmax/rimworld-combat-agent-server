fix: rename variable for clarity in DQN agent's next action calculation

agents/dqn/agent.py
@@ -225,17 +225,15 @@ class DQNAgent:
             ]
 
             with torch.no_grad():
-                next_atoms_batch = self.target_net.forward(stateN_batch)
+                T_atoms_batch = self.target_net.forward(stateN_batch)
                 next_action_batch = (
-                    self._get_expected_q_values(next_atoms_batch)
-                    .argmax(dim=1)
-                    .squeeze()
+                    self._get_expected_q_values(T_atoms_batch).argmax(dim=1).squeeze()
                 )
-                T_atoms_batch = next_atoms_batch[
-                    torch.arange(next_atoms_batch.size(0)), next_action_batch, :
+                T_dist_batch = T_atoms_batch[
+                    torch.arange(T_atoms_batch.size(0)), next_action_batch, :
                 ]
                 T_dist_batch = self._project_distribution(
-                    T_atoms_batch,
+                    T_dist_batch,
                     rewardN_batch,
                     done_batch,
                 )
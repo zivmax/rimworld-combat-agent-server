f30108b1f8065274ae36f451184f4683aa8f59f9
zivmax <tanglzh2022@shanghaitech.edu.cn>
2025-01-17 00:43:49 +0000

    feat: update deployment scripts for PGAgent and PPOAgent with new environment options and reward structure

--- agents/pgm/deploy.py
+++ agents/pgm/deploy.py
@@ -1,60 +1,88 @@
 import gymnasium as gym
 from tqdm import tqdm
-from gymnasium.wrappers import RecordEpisodeStatistics, FrameStackObservation
+
 from agents.pgm import PGAgent as Agent
-from env import rimworld_env
-from utils.draw import draw
-from utils.timestamp import timestamp
+from env import rimworld_env, GameOptions, EnvOptions, register_keyboard_interrupt
+from env.wrappers import (
+    FrameStackObservation,
+    SwapObservationAxes,
+)
 
-ENV_OPTIONS = {
-    "interval": 0.5,
-    "speed": 2,
-    "action_range": 1,
-    "is_remote": False,
-    "remain_still_threshold": 4,
-    "rewarding": {
-        "original": 0,
-        "win": 0,
-        "lose": -0,
-        "ally_defeated": -10,
-        "enemy_defeated": 10,
-        "ally_danger": -10,
-        "enemy_danger": 10,
-        "invalid_action": -1,
-        "remain_still": 0,
-    },
-}
+N_EPISODES = int(5)  # Total number of steps to train for
+MODEL = "agents/dqn/models/2025-01-17_01:47:30/200000.pth"
 
-N_EPISODES = 1000
-PLOT_INTERVAL = 100
-MODEL_PATH = "agents/ppo/models/2024-12-28_19:19:28/ppo_800.pth"
+
+ENV_OPTIONS = EnvOptions(
+    action_range=1,
+    max_steps=300,
+    rewarding=EnvOptions.Rewarding(
+        original=0,
+        win=50,
+        lose=-50,
+        ally_defeated=0,
+        enemy_defeated=0,
+        ally_danger=-200,
+        enemy_danger=200,
+        invalid_action=-0.25,
+    ),
+    game=GameOptions(
+        agent_control=True,
+        team_size=1,
+        map_size=15,
+        gen_trees=True,
+        gen_ruins=True,
+        random_seed=4048,
+        can_flee=False,
+        actively_attack=False,
+        interval=0.5,
+        speed=1,
+    ),
+)
 
 
 def main():
-    env = gym.make(rimworld_env, options=ENV_OPTIONS)
-    env = FrameStackObservation(env, stack_size=4)
-    env = RecordEpisodeStatistics(env, buffer_length=N_EPISODES)
-    agent = Agent(obs_space=env.observation_space, act_space=env.action_space)
-    agent.policy.load(MODEL_PATH)
+    env = gym.make(
+        rimworld_env,
+        options=ENV_OPTIONS,
+        port=ENV_OPTIONS.game.server_port,
+        render_mode="human",
+    )
+    env = FrameStackObservation(env, stack_size=8)
+    env = SwapObservationAxes(env, swap=(0, 1))
+    register_keyboard_interrupt(env)
+    agent = Agent(
+        n_envs=1,
+        obs_space=env.observation_space,
+        act_space=env.action_space[0],
+        device="cuda:1",
+    )
+    agent.policy.load(MODEL)
     agent.policy.eval()
-    for episode in tqdm(range(1, N_EPISODES + 1), desc="Evaluating Progress"):
-        state, _ = env.reset()
-        done = False
-        episode_reward = 0
 
-        while not done:
-            action = agent.act(state)
+    next_state, _ = env.reset()
 
-            next_state, reward, terminated, truncated, _ = env.step(action)
-            done = terminated or truncated
+    done = False
+    rewards = [0] * N_EPISODES
+    with tqdm(total=N_EPISODES, desc="Testing (Episodes)") as pbar:
+        for i in range(N_EPISODES):
+            while not done:
+                current_state = next_state
+                actions = agent.act([current_state])
 
-            episode_reward += reward
-            state = next_state
+                action = {
+                    0: actions[0],
+                }
 
-        if episode % PLOT_INTERVAL == 0:
-            draw(env, save_path=f"agents/ppo/plots/{timestamp}/env_{episode}.png")
-
+                next_state, reward, terminated, truncated, _ = env.step(action)
+                rewards[i] += reward
+                done = terminated or truncated
+                if done:
+                    break
+            done = False
+            next_state, _ = env.reset()
+            pbar.update(1)
     env.close()
+    print(rewards)
 
 
 if __name__ == "__main__":
